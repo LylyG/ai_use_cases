9056b1b0de896db401390532bbd4a6f934f320c0
Lyrechel Galarza-Punter <lgalarzapunter@gmail.com>
2016-08-31 01:23:34 -0400

    Adding jQuery to hide image and show video

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-$(document).foundation()
+$(document).ready(function() {
+  $( "#play" ).on( "click", function(event) {
+      $( "#play" ).hide();
+      $( ".flex-video" ).show();
+      $( ".flex-video" )[0].src += "&autoplay=1"; event.preventDefault();
+  });
+});
